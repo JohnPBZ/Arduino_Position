55788af5c8b079574edf2355d32d64adbd5a622a
PBZ <t107360119@ntut.org.tw>
2021-02-04 12:56:11 +0800

    從一次10筆改成一次一筆，但頻率大幅下降，約10~20Hz一筆資料 還沒加運動軌跡資訊

--- displayMessage.m
+++
@@ -1,9 +0,0 @@
-function displayMessage(msg,title)
-close;
-CreateStruct.Interpreter = 'tex';
-CreateStruct.WindowStyle = 'modal';
-msg= strcat('\fontsize{10}',msg,'.Click OK to continue.');
-dlg =msgbox(msg,title,CreateStruct);
-waitfor(dlg);
-end
-
